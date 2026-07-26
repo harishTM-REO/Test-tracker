774674155f18d9c8913eb8744659b0069903bdf1
harish <harishthirumurthi@gmail.com>
2026-07-26 21:06:43 +0530

    update rate limits

--- backend/adobe-target-1.0-worker/index.js
+++ backend/adobe-target-1.0-worker/index.js
@@ -54,7 +54,7 @@ app.use(compression());
 // Rate limiting scoped to API routes
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: 100,
+  max: 5000,
   message: {
     success: false,
     message: 'Too many requests from this IP, please try again later.'
